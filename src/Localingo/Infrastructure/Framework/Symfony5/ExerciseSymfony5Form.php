914317c588811b4e3d58a295c8fb7c1c5199e855
Adrien Felipe <adrien@macnificos.com>
2021-07-23 22:03:09 +0200

    Exercise form corrections as functions

--- src/Localingo/Infrastructure/Framework/Symfony5/ExerciseSymfony5Form.php
+++ src/Localingo/Infrastructure/Framework/Symfony5/ExerciseSymfony5Form.php
@@ -19,6 +19,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ExerciseSymfony5Form extends AbstractController implements ExerciseFormInterface
 {
+    private const STYLE_ROW_BIG = 'big';
+    private const STYLE_ROW_NORMAL = 'normal';
     private ExerciseValidation $exerciseValidation;
 
     public function __construct(ExerciseValidation $exerciseValidation)
@@ -31,88 +33,78 @@ class ExerciseSymfony5Form extends AbstractController implements ExerciseFormInt
         $request = Request::createFromGlobals();
         $exercise = new Exercise(ExerciseType::declined(), $sample);
 
-        $builder = $this->formBuilder($exercise);
-        $form = $builder->getForm();
-
+        $form = $this->formBuilder($exercise)->getForm();
         $form->handleRequest($request);
+
         // Form was submitted.
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var ExerciseDTO $submittedDTO */
             $submittedDTO = $form->getData();
             $corrections = $this->exerciseValidation->getCorrections($exercise, $submittedDTO);
             // Build a brand new form with all the values.
-            $builder = $this->formBuilder($exercise, $corrections);
-            foreach ($corrections as $property => $isCorrect) {
-                $item = $builder->get($property);
-                $options = $builder->get($property)->getOptions();
-                $class = $isCorrect ? 'is-valid' : 'is-invalid';
-                /** @psalm-suppress MixedArrayAccess **/
-                if (is_string($options['attr']['class'] ?? null)) {
-                    /** @psalm-suppress MixedOperand, MixedArrayAssignment **/
-                    $options['attr']['class'] .= " $class";
-                } else {
-                    /** @psalm-suppress MixedArrayAssignment **/
-                    $options['attr']['class'] = $class;
-                }
-                $builder->remove($property);
-                $type = $item->getType()->getInnerType()::class;
-                $builder->add($property, $type, $options);
-            }
-            $form = $builder->getForm();
+            $form = $this->formBuilder($exercise, $corrections)->getForm();
         }
 
         return $form->createView();
     }
 
     /**
-     * @param array<string, bool>|null $corrections
+     * @param array<string, bool> $corrections
      *
      * @psalm-suppress TooManyTemplateParams
      *
      * @return FormBuilderInterface<mixed,mixed>
      */
-    private function formBuilder(Exercise $exercise, array $corrections = null): FormBuilderInterface
+    private function formBuilder(Exercise $exercise, array $corrections = []): FormBuilderInterface
     {
         $isExercise = !$corrections;
+        $questions = $exercise->getQuestions();
         $exerciseDTO = $exercise->getDTO($isExercise);
 
+        $builder = $this->createFormBuilder($exerciseDTO);
+        $properties = $exercise->getDTO()->asPropertyNames();
+        $this->addTextRow($builder, (string) $properties->translation, self::STYLE_ROW_BIG, $isExercise, $questions, $corrections);
+        $this->addTextRow($builder, (string) $properties->declined, self::STYLE_ROW_BIG, $isExercise, $questions, $corrections);
+        $this->addTextRow($builder, (string) $properties->word, self::STYLE_ROW_NORMAL, $isExercise, $questions, $corrections);
+        $this->addTextRow($builder, (string) $properties->gender, self::STYLE_ROW_NORMAL, $isExercise, $questions, $corrections);
+        $this->addTextRow($builder, (string) $properties->state, self::STYLE_ROW_NORMAL, $isExercise, $questions, $corrections);
+        $this->addTextRow($builder, (string) $properties->case, self::STYLE_ROW_NORMAL, $isExercise, $questions, $corrections);
+        $builder->add('next', SubmitType::class, [
+            'label' => 'Check',
+            'row_attr' => ['class' => 'p-2 d-grid border-top'],
+        ]);
+
+        return $builder;
+    }
+
+    /**
+     * @psalm-suppress TooManyTemplateParams
+     *
+     * @param FormBuilderInterface<mixed> $builder
+     * @param string[]                    $questions
+     * @param array<string, bool>         $corrections
+     */
+    private function addTextRow(FormBuilderInterface $builder, string $name, string $style, bool $isExercise, array $questions, array $corrections): void
+    {
+        $style_classes = $style === self::STYLE_ROW_BIG ? 'form-control-lg text-success' : 'form-control-sm bg-white';
+        $isQuestion = in_array($name, $questions, true);
+
+        if ($isExercise || !$isQuestion) {
+            $correction_classes = '';
+        } else {
+            $correction_classes = $corrections[$name] ?? true ? 'is-valid' : 'is-invalid';
+        }
         $floatingConf = [
             'required' => false,
             'attr' => [
-                'class' => 'border-0 rounded-0 bg-light',
-                'disabled' => true,
-                'readonly' => true,
+                'class' => "border-0 rounded-0 bg-light $style_classes $correction_classes",
+                'disabled' => !($isExercise && $isQuestion),
+                'readonly' => !($isExercise && $isQuestion),
             ],
             'row_attr' => [
                 'class' => 'form-floating mb-1 border-top',
             ],
         ];
-        $whiteConf = $bigConf = $floatingConf;
-        $bigConf['attr']['class'] .= ' form-control-lg text-success';
-        $whiteConf['attr']['class'] .= ' form-control-sm bg-white';
-
-        // Empty fields will be editable.
-        $translationConf = $declinedConf = $bigConf;
-        $wordConf = $floatingConf;
-
-        // Translation input setup.
-        $isTranslation = $exercise->getType()->isTranslation() && $isExercise;
-        $translationConf['attr'] = array_merge($translationConf['attr'], ['disabled' => !$isTranslation, 'readonly' => !$isTranslation]);
-
-        // Declined input setup.
-        $isDeclined = $exercise->getType()->isDeclined() && $isExercise;
-        $declinedConf['attr'] = array_merge($declinedConf['attr'], ['disabled' => !$isDeclined, 'readonly' => !$isDeclined]);
-
-        return $this->createFormBuilder($exerciseDTO)
-            ->add('translation', TextType::class, $translationConf)
-            ->add('declined', TextType::class, $declinedConf)
-            ->add('word', TextType::class, $wordConf)
-            ->add('gender', TextType::class, $whiteConf)
-            ->add('state', TextType::class, $whiteConf)
-            ->add('case', TextType::class, $whiteConf)
-            ->add('next', SubmitType::class, [
-                'label' => 'Check',
-                'row_attr' => ['class' => 'p-2 d-grid border-top'],
-            ]);
+        $builder->add($name, TextType::class, $floatingConf);
     }
 }
