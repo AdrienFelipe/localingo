c162cf8cef94308a97412b39c857decf1963e56f
Adrien Felipe <adrien@macnificos.com>
2021-07-25 13:55:13 +0200

    Update same word exercises to skip 'new'

--- src/Localingo/Application/Exercise/ExerciseExecute.php
+++ src/Localingo/Application/Exercise/ExerciseExecute.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Localingo\Application\Exercise;
 
 use App\Localingo\Application\Experience\ExperienceExecute;
+use App\Localingo\Domain\Episode\Episode;
 use App\Localingo\Domain\Exercise\Exception\ExerciseMissingStateOrder;
 use App\Localingo\Domain\Exercise\Exercise;
 
@@ -24,8 +25,23 @@ class ExerciseExecute
     {
         // Update exercise.
         $isCorrect ? $exercise->nextState() : $exercise->previousState();
+        // Remove the state 'new' from all other same word exercises.
+        $this->updateExercises($exercise->getEpisode(), $exercise->getSample()->getWord());
 
         // Update experience.
         $this->experienceExecute->applyAnswer($exercise, $isCorrect);
+    }
+
+    /**
+     * @throws ExerciseMissingStateOrder
+     */
+    private function updateExercises(Episode $episode, string $word): void
+    {
+        // Remove the state 'new' from all other same word exercises.
+        foreach ($episode->getExercises() as $exercise) {
+            if ($exercise->getSample()->getWord() === $word && $exercise->getState()->isNew()) {
+                $exercise->nextState();
+            }
+        }
     }
 }
