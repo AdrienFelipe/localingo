667f0dc62d46549ab68ad08f50ac91858147fb7a
Adrien <adrien@macnificos.com>
2021-07-31 11:46:40 +0200

    Deprecation refactor: use session from 'RequestStack'

--- src/Shared/Infrastructure/Framework/Symfony5/Symfony5Session.php
+++ src/Shared/Infrastructure/Framework/Symfony5/Symfony5Session.php
@@ -5,15 +5,25 @@ declare(strict_types=1);
 namespace App\Shared\Infrastructure\Framework\Symfony5;
 
 use App\Shared\Domain\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface as SymfonySession;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
 class Symfony5Session implements SessionInterface
 {
     private SymfonySession $session;
 
-    public function __construct(SymfonySession $session)
+    public function __construct(RequestStack $requestStack)
     {
-        $this->session = $session;
+        try {
+            $this->session = $requestStack->getSession();
+        } catch (SessionNotFoundException) {
+            // Tests environment do not have an available session.
+            // TODO: configure the session mock as the session service for tests.
+            $this->session = new Session(new MockArraySessionStorage());
+        }
     }
 
     /**
